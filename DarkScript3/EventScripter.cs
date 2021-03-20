39c0c71c2496362f59e101b0b00927a652eca439
Cliff <aintunez@gmail.com>
2021-03-19 22:58:35 -0400

    Revert "because fromsoft doesn't clean up their stuff"
    This reverts commit 2ab73ffdc939d2bcf32d55605d753aef99cba109.

--- DarkScript3/EventScripter.cs
+++ DarkScript3/EventScripter.cs
@@ -317,7 +317,16 @@ namespace DarkScript3
                     {
                         if (IsVariableLength(doc))
                         {
-                            argStruct = Enumerable.Repeat(ArgType.Int32, ins.ArgData.Length / 4);
+                            var temp = new ArgType[ins.ArgData.Length / 4];
+                            temp[0] = ArgType.Int32;
+                            temp[1] = ArgType.UInt32;
+                            for (int i = 2; i < temp.Count(); i++)
+                            {
+                                temp[i] = ArgType.Int32;
+                            }
+
+                            argStruct = temp.AsEnumerable();
+
                             args = ins.UnpackArgs(argStruct).Select(a => a.ToString()).ToArray();
                             argString = ArgumentStringInitializer(args, insIndex, paramNames, argStruct);
                         }
